15e7da094d28afccbaf4802702d57380e3943d22
Axel M <Axel_mendez1@hotmail.com>
2019-11-15 05:54:01 -0300

    MigrationC#

---
+++ WebApplicationn/Data/ContactoContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationn.Models;
+
+namespace WebApplicationn.Data
+{
+    public class ContactoContexto : DbContext
+    {
+        public ContactoContexto(DbContextOptions<ContactoContexto>options):base(options)
+        {
+
+        }
+        //Crear nuestro dbset
+        public DbSet<Contacto> ContactoItems { get; set; }
+    }
+}
